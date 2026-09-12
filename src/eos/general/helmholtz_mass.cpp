1eac32e3f5dcd3ba2bc3ba36372fb80c5d5b4b69
Sho Fujibayashi <sho.fujibayashi@gmail.com>
2026-09-12 23:20:08 +0900

    Validate mexc scalar index before use

--- src/eos/general/helmholtz_mass.cpp
+++ src/eos/general/helmholtz_mass.cpp
@@ -1481,7 +1481,7 @@ void EquationOfState::InitEosConstants(ParameterInput *pin) {
   
   if (pin->DoesParameterExist("hydro", "helm_mexc_index")) {
     i_mexc = pin->GetInteger("hydro", "helm_mexc_index");
-    scalar_floor_disabled_[i_mexc] = true;
+
     if (i_mexc < 0 || i_mexc >= NSCALARS) {
       std::stringstream msg;
       msg << "### FATAL ERROR in EquationOfState::InitEosConstants" << std::endl
@@ -1489,6 +1489,9 @@ void EquationOfState::InitEosConstants(ParameterInput *pin) {
           << std::endl;
       ATHENA_ERROR(msg);
     }
+
+    scalar_floor_disabled_[i_mexc] = true;
+
     if (i_mexc == i_ye) {
       std::stringstream msg;
       msg << "### FATAL ERROR in EquationOfState::InitEosConstants" << std::endl
