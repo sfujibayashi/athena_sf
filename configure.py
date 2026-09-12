dcdd41f47f00d25eb390d8dba38e296a5e7bfdae
Sho Fujibayashi <sho.fujibayashi@gmail.com>
2026-09-12 10:47:28 +0900

    WIP: configure and macro modified to allow helm

--- configure.py
+++ configure.py
@@ -100,7 +100,7 @@ parser.add_argument(
 parser.add_argument('--eos',
                     default='adiabatic',
                     choices=['adiabatic', 'isothermal', 'general/eos_table',
-                             'general/hydrogen', 'general/ideal'],
+                             'general/hydrogen', 'general/ideal', 'general/helmholtz_mass'],
                     help='select equation of state')
 
 # --flux=[name] argument
@@ -469,8 +469,13 @@ else:
     definitions['GENERAL_EOS'] = '1'
     makefile_options['GENERAL_EOS_FILE'] = 'general'
     definitions['NHYDRO_VARIABLES'] = '5'
-    if args['eos'] == 'general/eos_table':
+    if args['eos'] in ['general/eos_table', 'general/helmholtz_mass']:
         definitions['EOS_TABLE_ENABLED'] = '1'
+definitions['MASS_EXCESS_ENERGY_ENABLED'] = \
+    '1' if args['eos'] == 'general/helmholtz_mass' else '0'
+
+definitions['HELMHOLTZ_EOS_ENABLED'] = \
+    '1' if args['eos'] == 'general/helmholtz_mass' else '0'
 
 # --flux=[name] argument
 definitions['RSOLVER'] = makefile_options['RSOLVER_FILE'] = args['flux']
