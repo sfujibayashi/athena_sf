4bdc53d7a8b7b3789daf82dc45a553148fb09194
Sho Fujibayashi <sho.fujibayashi@gmail.com>
2026-09-26 14:18:18 +0900

    FaceArea is multiplied with DensitizationFactor

--- src/coordinates/gr_dynamic.cpp
+++ src/coordinates/gr_dynamic.cpp
@@ -397,28 +397,34 @@ void Coordinates::CenterWidth3(const int k, const int j, const int il, const int
 
 void Coordinates::Face1Area(const int k, const int j, const int il, const int iu,
                               AthenaArray<Real> &areas) {
+  class Metric *pmetric = pmy_block->pmetric;
   //  \Delta A = r^2 (-\Delta\cos\theta) \Delta\phi
 #pragma omp simd
   for (int i=il; i<=iu; ++i)
-    areas(i) = coord_area1_i1_(i) * coord_area1_j1_(j) * dx3f(k);
+    areas(i) = coord_area1_i1_(i) * coord_area1_j1_(j) * dx3f(k) 
+      *pmetric->Face1DensitizationFactor(k,j,i);
   return;
 }
 
 void Coordinates::Face2Area(const int k, const int j, const int il, const int iu,
                               AthenaArray<Real> &areas) {
+  class Metric *pmetric = pmy_block->pmetric;
   // \Delta A = 1/3 \Delta(r^3) \sin\theta \Delta\phi
 #pragma omp simd
   for (int i=il; i<=iu; ++i)
-    areas(i) = coord_area2_i1_(i) * coord_area2_j1_(j) * dx3f(k);
+    areas(i) = coord_area2_i1_(i) * coord_area2_j1_(j) * dx3f(k)
+      *pmetric->Face2DensitizationFactor(k,j,i);
   return;
 }
 
 void Coordinates::Face3Area(const int k, const int j, const int il, const int iu,
                               AthenaArray<Real> &areas) {
+  class Metric *pmetric = pmy_block->pmetric;
   // \Delta A = 1/3 \Delta(r^3) (-\Delta\cos\theta)
 #pragma omp simd
   for (int i=il; i<=iu; ++i)
-    areas(i) = coord_area3_i1_(i) * coord_area3_j1_(j);
+    areas(i) = coord_area3_i1_(i) * coord_area3_j1_(j)
+      *pmetric->Face3DensitizationFactor(k,j,i);
   return;
 }
 
